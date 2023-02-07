bb0bcaa2acffaac9bba74ea1e6998ade48dbdf28
anaraa@code.edu.az <you@example.com>
2023-02-07 11:02:20 -0800

    Js

---
+++ assets/js/script.js
@@ -0,0 +1,279 @@
+"use strict"
+
+
+// // task-1
+
+
+// function isNumber(n){
+    
+//     if(n%3==0 && n%7==0){
+
+//         console.log("bolunur")
+//     }
+//     else{
+//         console.log("bolunmur")
+//     }
+
+
+// }
+
+// isNumber(21)
+
+
+
+// let isNumber = (n)=>{
+
+//     if(n%3==0 && n%7==0){
+
+//         console.log("bolunur")
+//     }
+//     else{
+//         console.log("bolunmur")
+//     }
+
+
+// }
+
+// isNumber(21)
+
+
+// // task-2
+
+// function factorial(num){
+//     if(num===0 || num===1)
+//     return 1;
+//     for(var i = num-1; i>=1;i--){
+//         num*=i;
+//     }
+//     console.log(num)
+// }
+
+// factorial(7)
+
+
+
+
+// let factorial = (num)=>{
+
+//     if(num===0 || num===1)
+//         return 1;
+
+//         for(var i = num-1; i>=1;i--){
+//             num*=i;
+//         }
+//     console.log(num)
+    
+
+
+// }
+
+// factorial(8)
+
+
+
+
+
+
+
+
+
+// // task-3
+
+
+// let nums = [1,2,3,4,5]
+
+// sumSqNumbers(nums)
+
+// function sumSqNumbers(arr){
+
+//     let result = 1;
+
+//     let sum = 0;
+
+//     for(let i = 0; i<arr.length; i++){
+//         if(arr[i] % 2 == 0){
+
+//             result=arr [i] **2;
+
+//             sum+=result;
+           
+//         }
+
+//     }
+    
+//     console.log(sum)
+
+// }
+
+
+// let nums = [1,2,3,4,5]
+
+// let sumSqNumbers = (arr) =>{
+    
+//       let result = 1;
+//       let sum = 0;
+
+
+
+//     for(let i=0; i<arr.length; i++){
+
+        
+
+//         if(arr[i] % 2 == 0){
+
+//             result = arr[i]**2;
+
+//             sum+=result;
+
+//         }
+
+//     }
+
+//     console.log(sum)
+// }
+
+// sumSqNumbers(nums)
+
+
+
+// // task-4
+
+
+
+
+
+// let mail = "cavid@code.edu.az";
+// let password ="12345";
+
+// function sumNums(mail,password){
+    
+//     if(mail == "cavid@code.edu.az" && password == "12345"){
+
+//         console.log("Girish olundu")
+//     }
+//     else{
+//         console.log("Mail ve yaxud password sehvdir")
+//     }
+
+
+// }
+
+// sumNums(mail,password)
+
+
+// let mail = "cavid@code.edu.az";
+// let password = "12345";
+
+// let sumNUms = (mail,password) =>{
+//     if(mail == "cavid@code.edu.az" && password == "12345"){
+
+//         console.log("Girish olundu")
+//     }
+//     else{
+//         console.log("Mail ve yaxud password sehvdir")
+//     }
+// }
+
+// sumNUms(mail,password)
+
+
+
+
+// // task-5
+
+// let nums = [1,2,3,4,5,6,7,8]
+
+// sumNums(nums)
+
+// function sumNums(arr){
+//     let sum = 0;
+
+//     for(let i = 0; i<arr.length; i++){
+//         if(arr[i]% 2 == 1){
+            
+//             sum+=arr[i];
+//         }
+        
+
+//     }
+//     console.log(sum)
+
+   
+// }
+
+
+// let nums=[1,2,3,4,5,]
+
+// let sumNums = (arr) =>{
+    
+//     let sum = 0;
+
+//     for(let i=0; i<arr.length; i++){
+        
+//         if(arr[i] % 2 == 1){
+
+//             sum+=arr[i];
+
+//         }
+//     }
+
+//     console.log(sum)
+// }
+
+// sumNums(nums)
+
+
+
+// // task-6
+
+
+// let nums = [2,3,4,5,8]
+
+// countNums(nums)
+
+
+// function countNums(arr){
+    
+//     let count=0;
+    
+//     for(let i = 0; i<arr.length; i++){
+        
+//         if(arr[i] % 2 == 0){
+            
+//             count++;
+            
+//         }
+
+//     }
+
+//     console.log(count)
+   
+// }
+
+
+
+// let nums = [2,3,4,5,8]
+
+
+// let countNums = (arr) =>{
+
+//     let count=0
+
+//     for(let i=0; i<arr.length;i++){
+//         if(arr[i]%2 == 0){
+
+//             count++;
+//         }
+        
+
+//     }
+//     console.log(count)
+
+// }
+
+// countNums(nums)
+
+
+
+
+
